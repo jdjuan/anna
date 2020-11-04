abbc224935d988975722df8a58ccb2badecc8ffd
Juan Herrera <david.juanherrera@gmail.com>
2020-11-04 20:24:02 +0100

    feat: add scroll down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnDestroy } from '@angular/core';
 import { Subject, timer } from 'rxjs';
-import { map, take, takeUntil, tap } from 'rxjs/operators';
+import { take, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2000ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnDestroy {
   readonly SPEED = 100;
@@ -14,6 +23,7 @@ export class AppComponent implements OnDestroy {
   fullTitle = 'Anna Mikulics'.split('');
   description = '';
   fullDescription = 'Creative and Scientific Writing.'.split('');
+  isTypewriterDone = false;
   destroy = new Subject();
 
   constructor() {
@@ -21,17 +31,25 @@ export class AppComponent implements OnDestroy {
   }
 
   initTypewriter(): void {
-    timer(0, this.SPEED).pipe(
-      take(this.fullTitle.length),
-      tap(() => (this.title += this.fullTitle.shift())),
-      takeUntil(this.destroy)
-    ).subscribe();
+    timer(0, this.SPEED)
+      .pipe(
+        take(this.fullTitle.length),
+        tap(() => (this.title += this.fullTitle.shift())),
+        takeUntil(this.destroy)
+      )
+      .subscribe();
     const delay = this.fullTitle.length * this.SPEED + this.DELAY_DESCRIPTION;
-    timer(delay, this.SPEED).pipe(
-      take(this.fullDescription.length),
-      tap(() => (this.description += this.fullDescription.shift())),
-      takeUntil(this.destroy)
-    ).subscribe();
+    timer(delay, this.SPEED)
+      .pipe(
+        take(this.fullDescription.length),
+        tap(() => (this.description += this.fullDescription.shift())),
+        takeUntil(this.destroy)
+      )
+      .subscribe({
+        complete: () => {
+          this.isTypewriterDone = true;
+        },
+      });
   }
 
   ngOnDestroy(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     NgbModule
   ],
   providers: [],
